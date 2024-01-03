8c27af5a892c5f9b26ac0e33745eca637c070dc2
Nicolas Fuentes <nfuentes11@gmail.com>
2024-01-03 14:47:11 -0500

    Web Server

--- files/tut05/server.js
+++ files/tut05/server.js
@@ -1,14 +1,20 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+
 const logEvents = require("./logEvents");
-
 const EventEmitter = require("events");
+class Emitter extends EventEmitter {}
+const myEmitter = new Emitter();
 
-class MyEmitter extends EventEmitter {}
+const PORT = process.env.PORT || 3500;
 
-const myEmitter = new MyEmitter();
+const server = http.createServer((request, response) => {
+  console.log(request.url, request.method);
+});
+// server lisetitng for request
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-myEmitter.on("log", (msg) => logEvents(msg));
-
-setTimeout(() => {
-  //emitt the event
-  myEmitter.emit("log", "Log event emitted!");
-}, 2000);
+// myEmitter.on("log", (msg) => logEvents(msg));
+// myEmitter.emit("log", "Log event emitted!");
