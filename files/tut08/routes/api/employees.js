d4004cb0f40fea8f11f494b49b6629c8a64afcaa
Nicolas Fuentes <nfuentes11@gmail.com>
2024-01-06 17:37:57 -0500

    Node/ExpresJs router

---
+++ files/tut08/routes/api/employees.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const data = {};
+data.employees = require("../../data/employees.json");
+
+router
+  .route("/")
+  .get((request, response) => {
+    response.json(data.employees);
+  })
+  .post((request, response) => {
+    response
+      .json({
+        firstname: request.body.firstname,
+        lastname: request.body.lastname,
+      }) // new emploee
+      .put((request, response) => {
+        response
+          .json({
+            firstname: request.body.firstname,
+            lastname: request.body.lastname,
+          })
+          .delete((request, response) => {
+            response.json({ id: request.body.id });
+          });
+      }); // updating emploeey
+  });
+
+//name paramiter directly out of the url
+router.route("/:id").get((request, response) => {
+  response.json({ id: request.params.id });
+});
+module.exports = router;
