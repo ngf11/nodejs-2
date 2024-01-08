034403b7b260b53ea547ef3af4ee05e7c9ccd54f
Nicolas Fuentes <nfuentes11@gmail.com>
2024-01-08 12:11:48 -0500

    authentication

---
+++ files/tut10/controller/registerController.js
@@ -0,0 +1,19 @@
+const usersDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const fsPromises = require("fs").promises;
+const path = require("path");
+const bcrypt = require("bcrypt");
+
+//handeler for new user information
+const handleNewUser = async (req, res) => {
+  const { user, pwd } = req.body;
+  if (!user || !pwd)
+    return res
+      .status(400)
+      .json({ message: "Username and password are required" });
+};
