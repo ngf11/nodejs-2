a9045cc8b92fd1e87b2c2f5bfcd212a0dffb5808
Nicolas Fuentes <nfuentes11@gmail.com>
2024-01-11 22:24:58 -0500

    Mongoose Data Model

--- files/tut14/model/Employee.js
+++ files/tut14/model/Employee.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
 const employeeSchema = new Schema({
   firstname: {
     type: String,
-    require: true,
+    required: true,
   },
   lastname: {
     type: String,
-    require: ture,
+    required: ture,
   },
 });
 
